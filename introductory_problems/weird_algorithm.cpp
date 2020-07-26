41c09843d4992fdce556b84dad88e26e4d3c6d58
pranshul narang <narang.pranshul@gmail.com>
2020-07-26 06:16:00 +0530

    file renamed

--- introductory_problems/weird_algorithm.cpp
+++ introductory_problems/weird_algorithm.cpp
@@ -1,33 +1,26 @@
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
-void search(int k,int n)
-{
-    vector<int> vecta;
-    if(k==n+1)
-    {
-        cout<<"vecta size is"<<vecta.size()<<"\n";
-        for(int i=0;i<vecta.size();i++)
-        {
-            cout<<vecta[i]<<" ";
-        }
-        cout<<"\n";
-    }
-    else
-    {
-        vecta.push_back(k);
-        cout<<"K is "<<k<<"\n";
-        cout<<"Vecta element 1 is "<<vecta[1]<<"\n";
-        search(k+1,3);
-        vecta.pop_back();
-        search(k+1,3);
-    }
-    
-}
-
 int main(){
-    int a[]={1,2,3};
-    search(1,3);
 
+    long long n;
+    cin>>n;
+    while(true)
+    {
+        cout<<n<<" ";
+        if(n==1)
+        break;
+        else
+        {
+            if(n%2==0)
+            n=n/2;
+            else
+            {
+                n=3*n+1;
+            }
+            
+        }
 
+        
+    }
+    //cout<<"\n";
 }
